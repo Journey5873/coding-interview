296124b4901cca61182f8257436408c81d7ff12b
inna <journeyin73@gmail.com>
2023-11-04 15:14:00 +0900

    Add [boj] 10828

---
+++ boj/10828.py
@@ -0,0 +1,26 @@
+import sys
+stack = []
+
+n = int(sys.stdin.readline())
+for _ in range(n):
+    command = sys.stdin.readline().split()
+
+    if command[0] == "push":
+        stack.append(command[1])
+    elif command[0] == "pop":
+        if len(stack) > 0:
+            print(stack.pop())
+        else:
+            print(-1)
+    elif command[0] == "size":
+        print(len(stack))
+    elif command[0] == "empty":
+        if len(stack) > 0:
+            print(0)
+        else:
+            print(1)
+    else:
+        if len(stack) > 0:
+            print(stack[-1])
+        else:
+            print(-1)
