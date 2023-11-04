095b03e826c6f386cd7b35a1c2ce13a57167c85b
inna <journeyin73@gmail.com>
2023-11-04 16:31:08 +0900

    Modified [boj] 10828

--- boj/10828.py
+++ boj/10828.py
@@ -6,21 +6,21 @@ for _ in range(n):
     command = sys.stdin.readline().split()
 
     if command[0] == "push":
-        stack.append(command[1])
+        stack.append(int(command[1]))
     elif command[0] == "pop":
-        if len(stack) > 0:
+        if stack:
             print(stack.pop())
         else:
             print(-1)
     elif command[0] == "size":
         print(len(stack))
     elif command[0] == "empty":
-        if len(stack) > 0:
+        if stack:
             print(0)
         else:
             print(1)
     else:
-        if len(stack) > 0:
+        if stack:
             print(stack[-1])
         else:
             print(-1)
