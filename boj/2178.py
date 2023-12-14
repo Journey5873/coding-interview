e8df8decf9787d32692db479d654462b46574f3a
inna <journeyin73@gmail.com>
2023-12-14 13:29:35 +0900

    Modified [boj] 2178

--- boj/2178.py
+++ boj/2178.py
@@ -1,32 +1,29 @@
 from collections import deque
-import sys
 
 rows, cols = map(int, input().split())
 maze = []
-
-for r in range(rows):
-    maze.append(list(map(int, input().rstrip())))
-dist = [[-1 for j in range(cols)] for i in range(rows)]
-
 dx = [1, 0, -1, 0]
 dy = [0, 1, 0, -1]
 
-def bfs(maze, dist, row, col):
-    queue = deque()
+for r in range(rows):
+    maze.append(list(map(int, input().strip())))
+dist = [[-1]*cols for _ in range(rows)]
 
-    queue.append((row, col))
-    dist[row][col] = 0
+def bfs(x, y):
+    queue = deque()
+    queue.append((x, y))
+    dist[x][y] = 0
 
     while queue:
         x, y = queue.popleft()
         for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
+            nx, ny = x + dx[i], y + dy[i] 
+
             if nx < 0 or nx >= rows or ny < 0 or ny >= cols:
                 continue
             if maze[nx][ny] == 1 and dist[nx][ny] == -1:
                 dist[nx][ny] = dist[x][y] + 1
                 queue.append((nx, ny))
-    
-bfs(maze, dist, 0, 0)
+
+bfs(0, 0)
 print(dist[rows-1][cols-1]+1)
