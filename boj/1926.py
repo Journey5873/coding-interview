0dbef7c98a60fca31e8f96a94516ab9ee2c6fe6b
inna <journeyin73@gmail.com>
2024-01-16 13:14:44 +0900

    Modified [boj] 1926

--- boj/1926.py
+++ boj/1926.py
@@ -1,43 +1,48 @@
 from collections import deque
 import sys
 
-rows, cols = map(int, sys.stdin.readline().split())
+rows, cols = map(int, input().split())
 drawing_paper = []
-total_drawing = 0
-max_area = 0
+widest_area = 0
+total_cnt = 0
+
+for r in range(rows):
+    drawing_paper.append(list(map(int, input().split())))
 
 dx = [1, 0, -1, 0]
 dy = [0, 1, 0, -1]
 
-for r in range(rows):
-    drawing_paper.append(list(map(int, sys.stdin.readline().split())))
-
-def bfs(drawing_paper, row, col):
+def bfs(r, c):
     queue = deque()
+    queue.append((r, c))
 
-    queue.append((row, col))
-    drawing_paper[row][col] = 0
-    area = 1
+    drawing_paper[r][c] = 0
+    area = 1 
 
     while queue:
         x, y = queue.popleft()
+
         for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
+            nx = dx[i] + x
+            ny = dy[i] + y
+
             if nx < 0 or nx >= rows or ny < 0 or ny >= cols:
                 continue
+
             if drawing_paper[nx][ny] == 1:
                 drawing_paper[nx][ny] = 0
                 queue.append((nx, ny))
                 area += 1
+
     return area
 
 for r in range(rows):
     for c in range(cols):
         if drawing_paper[r][c] == 1:
-            area = bfs(drawing_paper, r, c)
-            max_area = max(max_area, area)
-            total_drawing += 1
+            area = bfs(r, c)
+            widest_area = max(area, widest_area)
+            total_cnt += 1
 
-print(total_drawing)
-print(max_area)
+print(total_cnt)    
+print(widest_area)
+
