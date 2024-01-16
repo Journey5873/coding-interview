a6148af21f2fb391f425935b13805767364549b9
inna <journeyin73@gmail.com>
2024-01-16 14:52:10 +0900

    Modified [boj] 7576

--- boj/7576.py
+++ boj/7576.py
@@ -1,43 +1,39 @@
 from collections import deque
-import sys
 
-cols, rows = map(int, sys.stdin.readline().split())
-box = []
-
-for r in range(rows):
-    box.append(list(map(int, sys.stdin.readline().split())))
+cols, rows = map(int, input().split())
+box = [list(map(int, input().split())) for _ in range(rows)]
+queue = deque()
 
 dx = [1, 0, -1, 0]
 dy = [0, 1, 0, -1]
 
-queue = deque()
+def bfs():
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if nx < 0 or nx >= rows or ny < 0 or ny >= cols:
+                continue
+
+            if box[nx][ny] == 0:
+                box[nx][ny] = box[x][y] + 1
+                queue.append((nx, ny))
+
+def calculate_min_days():
+    min_day = 0
+    for r in range(rows):
+        for c in range(cols):
+            if box[r][c] == 0:
+                return -1
+            min_day = max(min_day, box[r][c])
+    return min_day - 1
+
 for r in range(rows):
     for c in range(cols):
         if box[r][c] == 1:
             queue.append((r, c))
 
-def bfs(box, queue, rows, cols):
-
-    while queue:
-        x, y = queue.popleft()
-
-        for i in range(4):
-            nx, ny = x + dx[i], y + dy[i]
-        
-            if nx < 0 or nx >= rows or ny < 0 or ny >= cols:
-                continue
-            if box[nx][ny] == 0:
-                queue.append((nx, ny))
-                box[nx][ny] = box[x][y] + 1
-                
-
-bfs(box, queue, rows, cols)
-
-count = 0
-for r in range(rows):
-    for c in range(cols):
-        if box[r][c] == 0:
-            print(-1)
-            exit()
-        count = max(count, box[r][c])
-print(count - 1)
+bfs()
+print(calculate_min_days())
