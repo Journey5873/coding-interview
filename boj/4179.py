5a761acc49633e06a8a4de0284968ac2ba391331
inna <journeyin73@gmail.com>
2024-01-23 20:51:01 +0900

    Modified [boj] 4179

--- boj/4179.py
+++ boj/4179.py
@@ -1,57 +1,59 @@
 from collections import deque
-import sys
+
+R, C = map(int, input().split())
+maze = [list(input().strip()) for _ in range(R)]
+
+j_dist = [[-1]*C for _ in range(R)]
+f_dist = [[-1]*C for _ in range(R)]
+
+j_queue = deque()
+f_queue = deque()
+
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
 
 def fire_bfs():
-    while fire_queue:
-        x, y = fire_queue.popleft()
-        for i in range(4):
-            nx, ny = x + dx[i], y + dy[i]
 
-            if nx < 0 or nx >= rows or ny < 0 or ny >= cols:
+    while f_queue:
+        x, y = f_queue.popleft()
+
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+
+            if nx < 0 or nx >= R or ny < 0 or ny >= C:
                 continue
 
             if maze[nx][ny] != "#" and f_dist[nx][ny] == -1:
-                fire_queue.append((nx, ny))
                 f_dist[nx][ny] = f_dist[x][y] + 1
+                f_queue.append((nx, ny))
 
 def j_bfs():
+
     while j_queue:
         x, y = j_queue.popleft()
 
         for i in range(4):
-            nx, ny = x + dx[i], y + dy[i]
+            nx = x + dx[i]
+            ny = y + dy[i]
 
-            if nx < 0 or nx >= rows or ny < 0 or ny >= cols:
+            if nx < 0 or nx >= R or ny < 0 or ny >= C:
                 return j_dist[x][y] + 1
 
-            if maze[nx][ny] == "." and j_dist[nx][ny] == -1 and (f_dist[nx][ny] == -1 or j_dist[x][y] + 1 < f_dist[nx][ny]):
-                j_queue.append((nx, ny))
+            if maze[nx][ny] != "#" and j_dist[nx][ny] == -1 and (j_dist[x][y] + 1 < f_dist[nx][ny] or f_dist[nx][ny] == -1):
                 j_dist[nx][ny] = j_dist[x][y] + 1
+                j_queue.append((nx, ny))
 
     return "IMPOSSIBLE"
 
-rows, cols = map(int, input().split())
-maze = []
-fire_queue = deque()
-j_queue = deque()
-dx = [1, 0, -1, 0]
-dy = [0, 1, 0, -1]
-
-for r in range(rows):
-    maze.append(list(sys.stdin.readline().strip()))
-j_dist = [[-1]*cols for _ in range(rows)]
-f_dist = [[-1]*cols for _ in range(rows)]
-
-for i in range(rows):
-    for j in range(cols):
-        if maze[i][j] == "F":
-            fire_queue.append((i, j))
-            f_dist[i][j] = 0
-        if maze[i][j] == "J":
-            j_queue.append((i, j))
-            j_dist[i][j] = 0
+for r in range(R):
+    for c in range(C):
+        if maze[r][c] == "J":
+            j_dist[r][c] = 0
+            j_queue.append((r, c))
+        elif maze[r][c] == "F":
+            f_dist[r][c] = 0 
+            f_queue.append((r, c))
 
 fire_bfs()
 print(j_bfs())
-
-
