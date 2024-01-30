df88bde7359b31dddcff310d749086cfe68f7f92
inna <journeyin73@gmail.com>
2024-01-30 15:10:34 +0900

    Modified [boj] 2493

--- boj/2493.py
+++ boj/2493.py
@@ -1,23 +1,19 @@
-import sys
-
-n = int(sys.stdin.readline())
-
-tops = list(map(int, sys.stdin.readline().split()))
+N = int(input())
+tops = list(map(int, input().split()))
 stack = []
 results = []
 
-for i in range(n):
+for i in range(len(tops)):
+
     while stack:
-        if tops[stack[-1]] <= tops[i]:
+        if tops[stack[-1]] < tops[i]:
             stack.pop()
         else:
-            results.append(stack[-1]+1)
+            results.append(stack[-1] + 1)
             break
 
-    # There are no tops higher than current top, so all the elements are poped 
     if not stack:
         results.append(0)
     stack.append(i)
-    
-for n in results:
-    print(n, end=" ")
+
+print(*results)
