cb557d2e309101c2f2496695f882f6aca56f2b4d
inna <journeyin73@gmail.com>
2024-01-30 14:05:52 +0900

    Modified [boj] 3273

--- boj/3273.py
+++ boj/3273.py
@@ -1,12 +1,14 @@
+from collections import defaultdict
+
 n = int(input())
 numbers = list(map(int, input().split()))
 target = int(input())
-
+visited = defaultdict(bool)
 count = 0
-visited = {}
+
 for num in numbers:
     diff = target - num
-    if diff in visited:
-        count += 1
+    count += visited[diff]
     visited[num] = True
+    
 print(count)
