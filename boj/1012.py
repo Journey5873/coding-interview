1dbb8ac355882ccbd77eebf2ddc1a468ba9c170d
inna <journeyin73@gmail.com>
2024-01-31 11:55:13 +0900

    Modified [boj] 1012

--- boj/1012.py
+++ boj/1012.py
@@ -1,45 +1,47 @@
 from collections import deque
 import sys
 
-case_num = int(input())
+input = sys.stdin.readline
+field = []
+
 dx = [1, 0, -1, 0]
 dy = [0, 1, 0, -1]
 
-def make_field(rows, cols):
-    field = [[0] * cols for _ in range(rows)]
-    return field
+T = int(input())
 
-def plant_cabbage(K):
-    for _ in range(K):
-        r, c = map(int, sys.stdin.readline().split())
-        field[r][c] = 1
+def plant_cabbage(k):
 
-def bfs(r, c):
-    queue = deque()
-    queue.append((r, c))
+    for _ in range(k):
+        x, y = map(int, input().split())
+        field[x][y] = 1
+
+def bfs():
 
     while queue:
         x, y = queue.popleft()
 
         for i in range(4):
-            nx, ny = x + dx[i], y + dy[i]
+            nx, ny = dx[i] + x, dy[i] + y
 
-            if nx < 0 or nx >= M or ny < 0 or ny >= N:
-                continue
-
-            if field[nx][ny] == 1:
-                queue.append((nx, ny))
+            if 0 <= nx < M and 0 <= ny < N and field[nx][ny] == 1:
                 field[nx][ny] = 0
+                queue.append((nx, ny))
 
-for _ in range(case_num):
-    M, N, K = map(int, sys.stdin.readline().split())
-    result = 0
-    field = make_field(M, N)
+
+for _ in range(T):
+
+    M, N, K = map(int, input().split())
+    queue = deque()
+    count = 0
+
+    field = [[0] * N for _ in range(M)]
     plant_cabbage(K)
-    
-    for r in range(M):
-        for c in range(N):
-            if field[r][c] == 1:
-                bfs(r, c)
-                result += 1
-    print(result)
+
+    for i in range(M):
+        for j in range(N):
+            if field[i][j] == 1:
+                field[i][j] = 0
+                queue.append((i, j))
+                bfs()
+                count += 1
+    print(count)
