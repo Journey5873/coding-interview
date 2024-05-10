91dbacce9b83bc5554534d3dde0f82898d70faa2
inna <journeyin73@gmail.com>
2024-05-10 15:28:05 +0900

    Modified [boj] 2573

--- boj/2573.py
+++ boj/2573.py
@@ -1,52 +1,58 @@
 from collections import deque
+import sys
+input = sys.stdin.readline
 
-rows, cols = map(int, input().split())
-graph = [list(map(int,input().split())) for _ in range(rows)]
-dx = [1, 0, -1, 0]
-dy = [0, 1, 0, -1]
-queue = deque()
-day = 0
-check = False
+N, M = map(int, input().split())
+matrix = [list(map(int, input().split())) for _ in range(N)]
 
-def bfs(a,b):
-    queue.append((a,b))
+def bfs(x, y, visited, sea_cnt):
+
+    dx = [1, 0, -1, 0]
+    dy = [0, 1, 0, -1]
+
+    queue = deque()
+    visited[x][y] = 1
+    queue.append((x, y))
+
     while queue:
-        x,y = queue.popleft()
-        visited[x][y] = True
+        x, y = queue.popleft()
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
-            if 0 <= nx < rows and 0 <= ny < cols:
-                if graph[nx][ny] != 0 and visited[nx][ny] == False:
-                    visited[nx][ny] = True
-                    queue.append((nx,ny))
-                elif graph[nx][ny] == 0:
-                    count[x][y] += 1
+
+            if 0 <= nx < N and 0 <= ny < M:
+                if matrix[nx][ny] > 0 and visited[nx][ny] == -1:
+                    visited[nx][ny] = 1
+                    queue.append((nx, ny))
+                elif matrix[nx][ny] == 0:
+                    sea_cnt[x][y] += 1
     return 1
 
-while True:
-    visited = [[False]*cols for _ in range(rows)]
-    count = [[0]*cols for _ in range(rows)]
-    result = []
-    for i in range(rows):
-        for j in range(cols):
-            if graph[i][j] != 0 and visited[i][j] == False:
-                result.append(bfs(i,j))
 
-    for i in range(rows):
-        for j in range(cols):
-            graph[i][j] -= count[i][j]
-            if graph[i][j] < 0:
-                graph[i][j] = 0
-    
-    if len(result) == 0: 
-        break
-    if len(result) >= 2: 
-        check = True
-        break
-    day += 1
+def solution():
+    day = 0
+    while True:
+        visited = [[-1] * M for _ in range(N)]
+        sea_cnt = [[0] * M for _ in range(N)]
+        ret = 0
 
-if check:        
-    print(day)
-else:
-    print(0)
+        for r in range(N):
+            for c in range(M):
+                if matrix[r][c] > 0 and visited[r][c] == -1:
+                    ret += bfs(r, c, visited, sea_cnt)
+        
+        if ret >= 2:
+            return day
+        elif ret == 0:
+            return 0
+        
+        for r in range(N):
+            for c in range(M):
+                matrix[r][c] -= sea_cnt[r][c]
+                if matrix[r][c] < 0:
+                    matrix[r][c] = 0
+        
+        day += 1
+
+
+print(solution())
