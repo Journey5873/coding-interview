f2887394a8e6fe60e81e6635f2cccfe8e851088a
inna <journeyin73@gmail.com>
2024-05-10 15:31:06 +0900

    Add [leetcode] 55

--- leetcode/55.py
+++ leetcode/55.py
@@ -3,10 +3,10 @@ class Solution(object):
         max_reach = 0
         for i, jump in enumerate(nums):
             if i > max_reach:
-                return False  # i가 max_reach를 넘으면 도달할 수 없으므로 False 반환
+                return False  
             max_reach = max(max_reach, i + jump)
             if max_reach >= len(nums) - 1:
-                return True  # 최대 도달 범위가 마지막 인덱스를 넘으면 True 반환
+                return True  
         return max_reach >= len(nums) - 1 
 
 s = Solution()
