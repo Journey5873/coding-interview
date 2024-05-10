40c9a344691547bc1b14b66eec5f7b1027d07a09
inna <journeyin73@gmail.com>
2024-05-10 15:28:25 +0900

    Modified [boj] 4179

--- boj/4179.py
+++ boj/4179.py
@@ -1,59 +1,52 @@
 from collections import deque
 
-R, C = map(int, input().split())
-maze = [list(input().strip()) for _ in range(R)]
-
-j_dist = [[-1]*C for _ in range(R)]
-f_dist = [[-1]*C for _ in range(R)]
+N, M = map(int, input().split())
+input_arr = [list(input()) for _ in range(N)]
 
 j_queue = deque()
-f_queue = deque()
+fire_queue = deque()
+j_dist = [[-1] * M for _ in range(N)]
+fire_dist = [[-1] * M for _ in range(N)]
 
 dx = [1, 0, -1, 0]
 dy = [0, 1, 0, -1]
 
 def fire_bfs():
-
-    while f_queue:
-        x, y = f_queue.popleft()
-
+    while fire_queue:
+        x, y = fire_queue.popleft()
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
 
-            if nx < 0 or nx >= R or ny < 0 or ny >= C:
-                continue
+            if nx < 0 or nx >= N or ny < 0 or ny >= M: continue
 
-            if maze[nx][ny] != "#" and f_dist[nx][ny] == -1:
-                f_dist[nx][ny] = f_dist[x][y] + 1
-                f_queue.append((nx, ny))
+            if input_arr[nx][ny] != "#" and fire_dist[nx][ny] == -1:
+                fire_dist[nx][ny] = fire_dist[x][y] + 1
+                fire_queue.append((nx, ny))
 
 def j_bfs():
-
     while j_queue:
         x, y = j_queue.popleft()
-
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
 
-            if nx < 0 or nx >= R or ny < 0 or ny >= C:
+            if nx < 0 or nx >= N or ny < 0 or ny >= M: 
                 return j_dist[x][y] + 1
-
-            if maze[nx][ny] != "#" and j_dist[nx][ny] == -1 and (j_dist[x][y] + 1 < f_dist[nx][ny] or f_dist[nx][ny] == -1):
+            
+            if input_arr[nx][ny] != "#" and j_dist[nx][ny] == -1 and (j_dist[x][y] + 1 < fire_dist[nx][ny] or fire_dist[nx][ny] == -1):
                 j_dist[nx][ny] = j_dist[x][y] + 1
                 j_queue.append((nx, ny))
+    return "IMPOSSIBLE"            
 
-    return "IMPOSSIBLE"
-
-for r in range(R):
-    for c in range(C):
-        if maze[r][c] == "J":
+for r in range(N):
+    for c in range(M):
+        if input_arr[r][c] == "F":
+            fire_dist[r][c] = 0
+            fire_queue.append((r, c))
+        if input_arr[r][c] == "J":
             j_dist[r][c] = 0
             j_queue.append((r, c))
-        elif maze[r][c] == "F":
-            f_dist[r][c] = 0 
-            f_queue.append((r, c))
 
 fire_bfs()
 print(j_bfs())
