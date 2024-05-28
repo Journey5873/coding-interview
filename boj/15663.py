e2d527d89269366356eb7b15e9b75cd1cdfb1c9e
inna <journeyin73@gmail.com>
2024-05-28 18:41:42 +0900

    Modified [boj] 15663

--- boj/15663.py
+++ boj/15663.py
@@ -5,9 +5,8 @@ N, M = map(int, input().split())
 nums = list(map(int, input().split()))
 arr = [0] * M
 isused = [False] * N
-
-
 nums.sort()
+
 def solution(k):
     if k == M:
         print(*arr)
@@ -16,9 +15,9 @@ def solution(k):
     latest = 0
     for i in range(N):
         if not isused[i] and latest != nums[i]:
-            isused[i] = True
             arr[k] = nums[i]
             latest = nums[i]
+            isused[i] = True
             solution(k+1)
             isused[i] = False
     return
