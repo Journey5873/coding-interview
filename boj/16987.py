f6904c822d5e12fb076942a65ce8f96e76df2b3f
inna <journeyin73@gmail.com>
2024-05-29 12:05:40 +0900

    [boj] Modified 16987

--- boj/16987.py
+++ boj/16987.py
@@ -1,37 +1,43 @@
-def backtracking(idx, broken_egg):
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+eggs = [list(map(int, input().split())) for _ in range(N)]
+max_egg = 0
+
+def solution(k, broken_egg):
     global max_egg
 
-    if idx == N:
+    if k == N:
         max_egg = max(max_egg, broken_egg)
         return
     
-    if egg[idx][0] <= 0 or broken_egg == N - 1:
-        backtracking(idx + 1, broken_egg)
+    if eggs[k][0] <= 0 or broken_egg == N - 1:
+        solution(k+1, broken_egg)
         return
-
-    copied_egg = broken_egg
+    
+    temp = broken_egg
     for i in range(N):
-        if i == idx or egg[i][0] <= 0:
+        
+        if k == i or eggs[i][0] <= 0:
             continue
 
-        egg[idx][0] -= egg[i][1]
-        egg[i][0] -= egg[idx][1]
+        eggs[k][0] -= eggs[i][1]
+        eggs[i][0] -= eggs[k][1]
 
-        if egg[i][0] <= 0:
+        if eggs[k][0] <= 0:
             broken_egg += 1
-        if egg[idx][0] <= 0:
+        if eggs[i][0] <= 0:
             broken_egg += 1
 
-        backtracking(idx + 1, broken_egg)
-        
-        egg[idx][0] += egg[i][1]
-        egg[i][0] += egg[idx][1]
-        broken_egg = copied_egg
-        
+        solution(k+1, broken_egg)
+
+        eggs[k][0] += eggs[i][1]
+        eggs[i][0] += eggs[k][1]
+
+        broken_egg = temp
+
     return max_egg
 
-N = int(input())
-egg = [list(map(int, input().split())) for _ in range(N)]
-max_egg = 0
-backtracking(0, 0)
+solution(0, 0)
 print(max_egg)
